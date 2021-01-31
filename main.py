4cad6e6c3b67d63a09d13e24fc4e4cf6e9ba6c8c
Danik <danykononenko45@gmail.com>
2021-01-31 03:12:43 +0300

    new background

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 import random
 
 pygame.init()
-size = WIDTH, HEIGHT = 800, 600
+size = WIDTH, HEIGHT = 840, 480
 screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 255))
 clock = pygame.time.Clock()
@@ -81,23 +81,10 @@ def terminate():
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
     fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, True, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+
 
     while True:
         for event in pygame.event.get():
@@ -202,7 +189,7 @@ tile_images = {
     'roof_4': load_image('roof_4.png'),
     'asphalt_luke': load_image('asphalt_luke.png')
 }
-player_image = load_image('gopnik_first.png')
+player_image = load_image('main.png')
 
 
 class Tile(pygame.sprite.Sprite):
